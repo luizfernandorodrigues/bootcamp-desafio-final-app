27d26ea766855c0ad9d7b502509461afefc76fd1
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-16 01:12:21 -0300

    Ajuste final nos titulos h1 e h2 da pagina

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ export default function App() {
   return (
     <div className="container">
       <div className="center">
-        <h1>Bootcamp Full Stack - Desafio Final</h1>
-        <h2>Bootcamp Full Stack - Desafio Final</h2>
+        <h1 style={{ fontSize: '2rem', fontWeight: '700' }}>
+          Bootcamp Full Stack - Desafio Final
+        </h1>
+        <h2 style={{ fontSize: '1.8rem' }}>
+          Bootcamp Full Stack - Desafio Final
+        </h2>
       </div>
       <Navigation />
     </div>
