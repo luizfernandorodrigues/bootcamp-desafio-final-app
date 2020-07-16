e66d6f69020f88b910ed1cf4139159f5bf6b93a2
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-15 23:24:45 -0300

    Centralização da div do titulo do app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div>
-      <h1>Bootcamp Full Stack - Desafio Final</h1>
-      <h2>Bootcamp Full Stack - Desafio Final</h2>
+    <div className="container">
+      <div className="center">
+        <h1>Bootcamp Full Stack - Desafio Final</h1>
+        <h2>Bootcamp Full Stack - Desafio Final</h2>
+      </div>
     </div>
   );
 }
