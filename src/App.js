5c265ca9d0bd804357ce186b8d4b5ec50f560664
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-15 23:14:15 -0300

    Modificado titulo da pagina inicial do app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function App() {
   return (
-    <h1>
-      Desafio Final do Bootcamp Full Stack - Luiz Fernando online no Heroku
-    </h1>
+    <div>
+      <h1>Bootcamp Full Stack - Desafio Final</h1>
+      <h2>Bootcamp Full Stack - Desafio Final</h2>
+    </div>
   );
 }
