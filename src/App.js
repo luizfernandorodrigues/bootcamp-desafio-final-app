f63faa27260d95e47c4d02fbd9b3b35fdd6e1cb6
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-07-18 12:29:49 -0300

    Implementado chamada para api e processamento dos periodos

--- src/App.js
+++ src/App.js
@@ -3,11 +3,59 @@ import Navigation from './components/Navigation/Navigation';
 import Totalizers from './components/Totalizers/Totalizers';
 import NewAndFilter from './components/NewAndFilter/NewAndFilter';
 import Values from './components/Values/Values';
+import * as api from './api/apiService';
+
+function sortTransactions(transactions) {
+  return transactions.sort((a, b) =>
+    a.yearMonthDay.localeCompare(b.yearMonthDay)
+  );
+}
+
+function getCurrentPeriod(allPeriods) {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const yearMonth = `${year}-${month.toString().padStart(2, '0')}`;
+  const currentPeriod = allPeriods.find(({ id }) => id === yearMonth);
+
+  return currentPeriod || Object.assign({}, allPeriods[0]);
+}
 
 export default function App() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [allPeriods, setAllPeriods] = useState([]);
   const [currentPeriod, setCurrentPeriod] = useState([]);
+  const [currentTransactions, setCurrentTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [selectedTransaction, setSelectedTransaction] = useState([]);
+  const [filterText, setFilterText] = useState('');
+  const [sumary, setSumary] = useState(null);
+
+  //carrega os periodos da api
+  useEffect(() => {
+    const getAllPeriods = async () => {
+      const data = await api.getAllPeriods();
+      setAllPeriods(data);
+
+      setCurrentPeriod(getCurrentPeriod(data));
+    };
+
+    getAllPeriods();
+  }, []);
+
+  //seta o periodo corrente
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!currentPeriod) {
+        return;
+      }
+
+      setCurrentTransactions([]);
+      const transactions = await api.getTransactionsFrom(currentPeriod);
+      setCurrentTransactions(transactions);
+    };
+    fetchData();
+  }, [currentPeriod]);
 
   const handlePeriodChange = (newPeiod) => {
     setCurrentPeriod(newPeiod);
--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'api' });
+const api = axios.create({
+  baseURL: 'https://luizfernando-desafio-final-api.herokuapp.com/',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
 const RESOURCE = '/transaction';
 
 const CURRENT_YEAR = new Date().getFullYear();
@@ -37,4 +42,40 @@ function _processPeriods() {
       allPeriods.push({ id, description: monthDescription, index: index++ });
     });
   });
+}
+
+function _prepareTransactions(transaction) {
+  const { description, category, _id, month, ...otherFields } = transaction;
+
+  return {
+    id,
+    description,
+    category,
+    month,
+    descriptionLowerCase: description.toLowerCase(),
+    categoryLowerCase: category.toLowerCase(),
+    monthDescription: MONTH_DESCRIPTIONS[month],
+    ...otherFields,
+  };
+}
+
+async function getAllPeriods() {
+  if (allPeriods.length === 0) {
+    _processPeriods();
+  }
+
+  return allPeriods;
+}
+
+async function getTransactionsFrom(period) {
+  const { id: yearMonth } = period;
+  const { data } = await api.get(`${RESOURCE}?period=${yearMonth}`);
+
+  const frontEndTransactions = data.transactions.map((x) => {
+    return _prepareTransactions(x);
+  });
+
+  return frontEndTransactions.sort((a, b) =>
+    a.yearMonthDay.localeCompare(b.yearMonthDay)
+  );
 }
